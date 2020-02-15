35ee43b0c51f6f61fe1131e5a67b6d62f618f8bf
Miras <peysho@mail.ru>
2020-02-15 23:16:59 +0300

    done?

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const http = require('http'),
+  url = require('url'),
+  fs = require('fs'),
+  { promisify } = require('util');
+  const readFilePromisify = promisify(fs.readFile);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const readFile = async(req, res, fileName) => {
+    await readFilePromisify(fileName, { encoding: 'utf8' }, (err, data) => {
+      if (err) throw err;
+      res.end(data);
+    });
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const server = () => {
+  http.createServer(async (req, res) => {
+    const routes = url.parse(req.url);
+    switch (routes.pathname) {
+      case "/":
+        readFile(req, res, './pages/index.html');
+        break;
+      case "/news":
+        readFile(req, res, './pages/news.html');
+        break;
+      case "/weather":
+        readFile(req, res, './pages/weather.html');
+        break;
+      case "/secret-stuff":
+        readFile(req, res, './pages/secret-stuff.html');
+        break;
+      case "/settings":
+        readFile(req, res, './pages/settings.html');
+        break;
+      default:
+        readFile(req, res, './pages/index.html');
+        break;
+    }
+  }).listen(3000);
+};
+
+server();
