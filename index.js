de5079db76ad1aa03ca0e57a20858df083d2ac76
Inheritor <peysho@Mail.ru>
2020-02-18 11:05:45 +0300

    Replace callbacks to async/await

--- index.js
+++ index.js
@@ -2,42 +2,45 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
+const { promisify } = require('util');
+const readFilePromisify = promisify(fs.readFile);
+const existsPromisify = promisify(fs.exists);
+const statPromisify = promisify(fs.stat);
 
 const fileExtension = {
   '.html': 'text/html',
   '.js': 'text/javascript',
-  '.json': 'application/json',
+  '.json': 'application/json'
 };
 
+http
+  .createServer(async (req, res) => {
+    const parsedUrl = url.parse(req.url);
 
-http.createServer(function (req, res) {
-  const parsedUrl = url.parse(req.url);
+    const filterPathExit = path
+      .normalize(parsedUrl.pathname)
+      .replace(/^(\.\.[\/\\])+/, '');
+    let pathname = path.join(__dirname, filterPathExit);
 
-  const filterPathExit = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, '');
-  let pathname = path.join(__dirname, filterPathExit);
-
-  fs.exists(pathname, function (exist) {
-    if(!exist) {
+    const exist = await existsPromisify(pathname);
+    if (!exist) {
       res.statusCode = 404;
       res.end(`File ${pathname} not found!`);
       return;
     }
-
-    if (fs.statSync(pathname).isDirectory()) {
-      pathname += '/index.html';
+    if ((await statPromisify(pathname)).isDirectory()) {
+      pathname += 'index.html';
     }
-
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        const ext = path.parse(pathname).ext;
-        res.setHeader('Content-type', fileExtension[ext] || 'text/plain' );
-        res.end(data);
-      }
-    });
+    try {
+      const data = await readFilePromisify(pathname);
+      const ext = path.parse(pathname).ext;
+      res.setHeader('Content-Type', fileExtension[ext] || 'text/plain');
+      res.end(data);
+    } catch (err) {
+      res.statusCode = 500;
+      res.end(`Error getting the file: ${err}.`);
+    }
+  })
+  .listen(3000, () => {
+    console.log('Server is running on localhost:3000/');
   });
-}).listen(3000, () => {
-  console.log('Server is running on localhost:3000/');
-});
